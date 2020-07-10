c17315fb5a3bd486f29fe45b5981df3281ee98c3
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-10 16:14:29 +0200

    Fix expcetion when the object does not respond to the input attribute name.

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -1,13 +1,17 @@
 class DatePickerInput < SimpleForm::Inputs::StringInput
   def input_html_options
-    value = object.send(attribute_name)
     options = {
-      value: value.respond_to?(:to_date) ? I18n.localize(value.to_date, format: :date_picker) : nil,
       data: {
         behaviour: 'date_picker',
         'date-format':  I18n.t('date.formats.date_picker_js')
       }
     }
+
+    value = if object.respond_to?(attribute_name) && object.send(attribute_name).respond_to?(:to_date)
+      I18n.localize(object.send(attribute_name).to_date, format: :date_picker)
+    end
+
+    options[:value] = value if value.present?
     options[:data][:'side-by-side'] =  @options[:'side_by_side'] || false
     options[:data][:'icon-provider'] = case @options[:icons]
     when :font_awesome
--- app/inputs/date_time_picker_input.rb
+++ app/inputs/date_time_picker_input.rb
@@ -1,13 +1,18 @@
 class DateTimePickerInput < SimpleForm::Inputs::StringInput
   def input_html_options
-    value = object.send(attribute_name)
     options = {
-      value: value.nil? ? nil : I18n.localize(value, format: :date_picker),
       data: {
         behaviour: 'date_picker',
         'date-format':  I18n.t('time.formats.date_picker_js')
       }
     }
+
+    value = if object.respond_to?(attribute_name)
+      I18n.localize(object.send(attribute_name), format: :date_picker)
+    end
+
+    options[:value] = value if value.present?
+
     options[:data][:'side-by-side'] =  @options[:'side_by_side'] || false
     options[:data][:'icon-provider'] = case @options[:icons]
     when :font_awesome
